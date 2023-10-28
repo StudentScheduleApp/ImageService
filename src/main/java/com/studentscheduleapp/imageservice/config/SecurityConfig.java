d7962b5410bdba54f9933a8e9573c674a4dac698
IlyaKrn <ilyakornoukhov@gmail.com>
2023-10-28 15:51:26 +0300

    refactor

--- src/main/java/com/studentscheduleapp/imageservice/config/SecurityConfig.java
+++ src/main/java/com/studentscheduleapp/imageservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.studentscheduleapp.imageservice.security.ServiceTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests(
                         authz -> authz
+                                .antMatchers(HttpMethod.GET, "/api/{id}").permitAll()
                                 .anyRequest().authenticated()
                                 .and()
                                 .addFilterAfter(serviceTokenFilter, BasicAuthenticationFilter.class)
