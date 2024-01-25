a70f2dcd936134a67a7a7d7d1b70cdf925583f5f
IlyaKrn <ilyakornoukhov@gmail.com>
2024-01-25 15:39:31 +0300

    multipart request

--- src/main/java/com/studentscheduleapp/imageservice/repos/DriveRepo.java
+++ src/main/java/com/studentscheduleapp/imageservice/repos/DriveRepo.java
@@ -2,9 +2,13 @@ package com.studentscheduleapp.imageservice.repos;
 
 import com.studentscheduleapp.imageservice.properties.services.DriveServiceProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,7 +22,11 @@ public class DriveRepo {
     private RestTemplate restTemplate;
 
     public String create(MultipartFile file) throws Exception {
-        ResponseEntity<String> r = restTemplate.postForEntity(driveServiceProperties.getUri() + driveServiceProperties.getUploadPath(), file, String.class);
+        Resource multipartFile = file.getResource();
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", multipartFile);
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, null);
+        ResponseEntity<String> r = restTemplate.postForEntity(driveServiceProperties.getUri() + driveServiceProperties.getUploadPath(), requestEntity, String.class);
         if(r.getStatusCode().is2xxSuccessful())
             return r.getBody();
         throw new Exception("request to " + driveServiceProperties.getUri() + " return code " + r.getStatusCode());
